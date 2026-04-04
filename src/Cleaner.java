bd7454d692b30f1fa849969e3fc344b8b82c0564
Dhruvraj Singh Gaur <rajeshreegaur42@gmail.com>
2026-04-04 15:35:21 +0530

    Delete src/Cleaner.java

--- src/Cleaner.java
+++
@@ -1,7 +0,0 @@
-import java.io.File;
-
-public class Cleaner {
-    public static void clean(String className){
-        new File("temp/" + className + ".java").delete();
-        new File("temp/" + className + ".class").delete();    }
-}
